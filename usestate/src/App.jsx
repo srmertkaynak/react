be47651beba8ecb4db81b22b62cb90ffd3969ea1
Mert Kaynak <kaynak.mert35@gmail.com>
2023-11-25 20:22:22 +0300

    UseState

---
+++ usestate/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [value, setValue] = useState(0);
+  const handleClick = () => {
+    setValue(value + 1);
+  };
+  return (
+    <div className="App">
+      <button onClick={handleClick}>Kurs Ekle</button>
+      <div>Kurs Sayısı: {value}</div>
+    </div>
+  );
+}
+
+export default App;
